93c7b554648f059fb7ea702899b376bb75c6ca2a
Ignacio Abal <iaba@guidewire.com>
2021-07-01 17:32:04 -0300

    user register WIP.

--- client/src/components/users/Register/RegisterForm/RegisterForm.jsx
+++ client/src/components/users/Register/RegisterForm/RegisterForm.jsx
@@ -2,18 +2,36 @@ import React, { Component } from "react";
 import Image from "./UserImage";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import axios from 'axios'
 import {Formik} from 'formik'
 
 export default class RegisterForm extends Component {
+  state={
+    username: '',
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    country: ''
+  }  
+
+  handleChange = event => {
+    // this.setState({ {event.target.id}: )
+  }
+
+  handleSubmit = event => {
+
+  }
+
   render() {
     return (
       <>
-        <Form>
+        <Form onSubmit={this.handleSubmit}>
           <h3>Create New Account</h3>
           <Image></Image>
           <Form.Group>
             <Form.Label>Username</Form.Label>
-            <Form.Control ></Form.Control>
+            <Form.Control onChange={} ></Form.Control>
           </Form.Group>
           <Form.Group>
             <Form.Label>E-mail</Form.Label>
@@ -50,7 +68,7 @@ export default class RegisterForm extends Component {
           <Form.Group>
             <Form.Check type="checkbox" label="I agree MYtinerary's terms and conditions." />
             <Form.Text className="text/muted"><a href="#">Terms & Conditions</a></Form.Text>
-            <Button type='submit'> Submit </Button>
+            <Button className="btnUserSubmit" type='submit' > Submit </Button>
           </Form.Group>
         </Form>        
       </>
